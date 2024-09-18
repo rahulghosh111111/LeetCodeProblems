dbddf7dc294decb4db35d730eff8be7b4867478f
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-18 15:42:43 +0000

    CountCompleteTreeNodes222

---
+++ CountCompleteTreeNodes222.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ListIterator;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class CountCompleteTreeNodes222 {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+       return countNodes(root.left) + countNodes(root.right) + 1;
+   }
+
+    public static void main(String[] args) {
+        CountCompleteTreeNodes222 p = new CountCompleteTreeNodes222();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        System.out.println(p.countNodes(root));
+    }
+}
