45fb52bdcf64fbda9c65382f7d8def16eb03b4c9
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-18 15:51:05 +0000

    CountCompleteTreeNodes222

--- CountCompleteTreeNodes222.java
+++ CountCompleteTreeNodes222.java
@@ -27,6 +27,9 @@ public class CountCompleteTreeNodes222 {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
         System.out.println(p.countNodes(root));
     }
 }
