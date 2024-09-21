ee3ecb5d03639890456a0c518778ad07f82d23a7
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-21 15:20:24 +0000

    LongestPalindromicSubstring5

---
+++ LongestPalindromicSubstring5.java
@@ -0,0 +1,40 @@
+public class LongestPalindromicSubstring5 {
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
+                }
+            }
+        }
+
+        return maxStr;
+    }
+
+    private boolean isPalindrome(String string) {
+        int left = 0;
+        int right = string.length() - 1;
+
+        while (left < right) {
+            if (string.charAt(left) != string.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring5 p = new LongestPalindromicSubstring5();
+        System.out.println(p.longestPalindrome("babad"));
+    }
+}
