5eb1599974a96d996b0bd9aab4c8993ecd29c39f
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-10-03 20:05:50 +0530

    Reverse Integer 7

---
+++ ReverseInteger7.java
@@ -0,0 +1,18 @@
+public class ReverseInteger7 {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger7 p = new ReverseInteger7();
+        System.out.println(p.reverse(123));
+    }
+}
