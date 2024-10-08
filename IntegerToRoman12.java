000b9596ad96c2839df39f4242c4c5f87d498e8b
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-10-08 22:04:09 +0530

    IntegerToRoman12

---
+++ IntegerToRoman12.java
@@ -0,0 +1,22 @@
+public class IntegerToRoman12 {
+    public String intToRoman(int num) {
+        int[] n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] s = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int i =0;
+        String str = new String(); 
+        while (num>0){
+            if (num>=n[i]){
+                str=str+s[i];
+                num-=n[i];
+            } else {
+                i++;
+            }
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman12 obj = new IntegerToRoman12();
+        System.out.println(obj.intToRoman(3749));
+    }
+}
