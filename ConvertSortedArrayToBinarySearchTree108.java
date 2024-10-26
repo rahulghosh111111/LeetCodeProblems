d77323202db6786e08fe1ed89171a03aafdb6348
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-10-26 23:22:51 +0530

    ConvertSortedArrayToBinarySearchTree108

---
+++ ConvertSortedArrayToBinarySearchTree108.java
@@ -0,0 +1,33 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class ConvertSortedArrayToBinarySearchTree108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+    public static void main(String[] args) {
+        ConvertSortedArrayToBinarySearchTree108 obj = new ConvertSortedArrayToBinarySearchTree108();
+        TreeNode root = obj.sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+        System.out.println(root);
+    }
+}
+// this code run in leetcode but not run here
